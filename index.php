d72860e1c981ae8966b694281078f3655358e354
talseb1 <talseb0@hotmail.com>
2015-10-26 14:01:25 +0100

    1e
    kkk

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Media queries 2</title>
+    </head>
+
+    <body>
+        <div id="page">
+            <header class="img_header">
+
+            </header>
+            <div class="row">
+                <div class="col-md-1"><button type="button">Accueil</button></div>
+                <div class="col-md-1"><button type="button">Notre confort</button></div>
+                <div class="col-md-1"><button type="button">Infospratiques</button></div>
+                <div class="col-md-1"><button type="button">Réservations</button></div>
+                <div class="col-md-1"><button type="button">Galeriephotos</button></div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-8">.col-md-8</div>
+                <div class="col-md-4">.col-md-4</div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">.col-md-4</div>
+                <div class="col-md-4">.col-md-4</div>
+                <div class="col-md-4">.col-md-4</div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">.col-md-6</div>
+                <div class="col-md-6">.col-md-6</div>
+            </div>
+
+        </div>
+        <footer>Editeur responsable felix@pension-sitting.com</footer>
+    </body>
+</html>
